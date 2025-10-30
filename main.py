708fca2f45c45ea557f06c2d766641fbfc97c16c
BORSAcoincisi <134699068+BORSAcoincisi@users.noreply.github.com>
2025-10-30 21:43:21 +0300

    Fix memory leak in event handlers

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2025-10-30T18:43:22.098Z
-Commit: Fix broken links in documentation
+Timestamp: 2025-10-30T18:43:23.754Z
+Commit: Fix memory leak in event handlers
