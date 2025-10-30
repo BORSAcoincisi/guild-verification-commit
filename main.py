191d948992cdd339fd97c2c15108ba6037365dd1
BORSAcoincisi <134699068+BORSAcoincisi@users.noreply.github.com>
2025-10-30 21:43:19 +0300

    Fix broken links in documentation

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2025-10-30T18:41:55.787Z
-Commit: Resolve timezone issues in date handling
+Timestamp: 2025-10-30T18:43:22.098Z
+Commit: Fix broken links in documentation
